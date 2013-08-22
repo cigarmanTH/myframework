dbf86e246ad3a2292e37ea09906b3e63e7a75a4c
Tomoyoshi Hirata <hiratatom@gmail.com>
2013-08-22 17:39:14 +0900

    gactget.py add

--- gactget.py
+++ gactget.py
@@ -1,16 +1,9 @@
-#!bin/python
+#!/usr/local/pythonz/ENV/Python-2.7.3/bin/python
 # coding: utf-8 
-
-# at first time in new environment,run plus_cli.py to get credentials before run this script
-import sys
-import codecs
-#sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 import apiclient.discovery
 import httplib2
 import settings 
-import simplejson as json
-
 import logging
 #logging.basicConfig()
 logging.getLogger().setLevel(getattr(logging, 'ERROR'))
@@ -23,7 +16,7 @@ def build_service(credentials, http, api_key=None):
     service = apiclient.discovery.build('plus', 'v1', http=http, developerKey=api_key)
     return service
 
-def activityprint(ggtsid):
+def gactget(ggtsid):
 
     httpUnauth = httplib2.Http()
     serviceUnauth = build_service(None, httpUnauth, settings.API_KEY)
@@ -33,20 +26,5 @@ def activityprint(ggtsid):
     
     activity = request.execute(httpUnauth)
     activities += activity['items']
-    
-    if len(activities) > 0:
-        for item in activities:
-            item['_id'] = item['id'] 
-            pp(item)
 
-def pp(obj):
-    if isinstance(obj, list) or isinstance(obj, dict):
-        orig = json.dumps(obj, indent=4)
-#        orig = json.dumps(obj)
-        print eval("u'''%s'''" % orig).encode('utf-8')
-    else:
-        print obj
-        
-if __name__=='__main__':
-    for ggtsid in ggtsidset.ggtsids:
-        activityprint(ggtsid)
+    return activities
